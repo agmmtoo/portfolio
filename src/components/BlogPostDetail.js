838e824ec8ffcd3dd4f70aaa3ddda5ea6dedac68
Aung Myint Myat Oo <solidifyarmor@gmail.com>
2021-11-09 19:32:48 +0630

    blog inner design ok

--- src/components/BlogPostDetail.js
+++ src/components/BlogPostDetail.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { toTitle } from '../formatter';
 import Fetch from './hooks/Fetch';
 import ReactMarkdown from 'react-markdown';
@@ -7,17 +7,22 @@ const BlogPostDetail = () => {
     const { folder, title } = useParams();
     const Title = toTitle(title);
     return (
-        <Fetch
-            url={`https://raw.githubusercontent.com/agmyintmyatoo/BlogPosts/main/${folder}/${Title}`}
-            renderData={renderMarkdown}
-        />
+        <div className="blog-post">
+            <Link to={`/blog`}>{folder}</Link> &gt; {title}
+            <hr />
+            <Fetch
+                url={`https://raw.githubusercontent.com/agmyintmyatoo/BlogPosts/main/${folder}/${Title}`}
+                renderData={renderMarkdown}
+            />
+        </div>
     )
 };
 
-const renderMarkdown = ({ data }) => {
+const renderMarkdown = data => {
     console.log(data);
+    const { data: md } = data;
     return (
-        <ReactMarkdown children={data} />
+        <ReactMarkdown children={md} />
     );
 }
 
